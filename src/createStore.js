6a7a0dd374fc76f97973a0aad4d8a54eebdf1b7e
Pedro Acosta <pedro.acosta613@gmail.com>
2017-10-08 00:43:53 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,17 @@
+import { render } from './index.js';
+export default function createStore(reducer) {
 
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState(){
+    return state;
+  }
+
+  return {dispatch, getState};
+
+}
